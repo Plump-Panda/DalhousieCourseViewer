750eb02b666615c7006069febda5f7c26653a7b8
Joshua Fung <formygit@gmail.com>
2020-09-09 20:17:20 -0700

    Added coloured borders to the cards

--- src/cardRenderer.js
+++ src/cardRenderer.js
@@ -1,9 +1,12 @@
 import React, {Component} from 'react';
 import { Card } from 'react-bootstrap';
 
-function getCard(code,name,description,creditHours){
+function getCard(code,name,description,creditHours,borderColour){
+    if(description == ""){
+        description = "No description available";
+    }
     return(
-        <Card style={{marginBottom: '2.5%'}}>
+        <Card style={{marginBottom: '2.5%'}} border={borderColour}>
             <Card.Header>{code}</Card.Header>
             <Card.Body>
                 <Card.Title>{name}</Card.Title>
@@ -21,64 +24,59 @@ export const makeCard = (json, courseLevel) => {
 
         //This looks hella dumb, probs a better way to do it
         switch(courseLevel){
-            //Will get all the courses
-            case 0:
-                return(
-                    getCard(code,name,description,creditHours)
-                )
             case 1:
                 if(check === '1'){
                     return(
-                        getCard(code,name,description,creditHours)
+                        getCard(code,name,description,creditHours,'primary')
                     )
                  }
                  break;
             case 2:
                 if(check === '2'){
                     return(
-                        getCard(code,name,description,creditHours)
+                        getCard(code,name,description,creditHours,'success')
                     )
                  }
                  break;
             case 3:
                 if(check === '3'){
                     return(
-                        getCard(code,name,description,creditHours)
+                        getCard(code,name,description,creditHours,'danger')
                     )
                  }
                  break;
             case 4:
                 if(check === '4'){
                     return(
-                        getCard(code,name,description,creditHours)
+                        getCard(code,name,description,creditHours,'warning')
                     )
                  }
                  break;
             case 6:
                 if(check === '6'){
                     return(
-                        getCard(code,name,description,creditHours)
+                        getCard(code,name,description,creditHours,'secondary')
                     )
                  }   
                  break;  
             case 7:
                 if(check === '7'){
                     return(
-                        getCard(code,name,description,creditHours)
+                        getCard(code,name,description,creditHours,'light')
                     )
                  }
                  break;
             case 8:
                 if(check === '8'){
                     return(
-                        getCard(code,name,description,creditHours)
+                        getCard(code,name,description,creditHours,'info')
                     )
                  }
                  break;
             case 9:
                 if(check === '9'){
                     return(
-                        getCard(code,name,description,creditHours)
+                        getCard(code,name,description,creditHours,'dark')
                     )
                  }
                  break;
--- src/components/all-courses.component.js
+++ src/components/all-courses.component.js
@@ -13,7 +13,14 @@ export default class ThirdYear extends Component{
         return(
             <div>
                 <h1 id='title'>All Courses</h1>
-                {makeCard(this.state,0)}
+                {makeCard(this.state,1)}
+                {makeCard(this.state,2)}
+                {makeCard(this.state,3)}
+                {makeCard(this.state,4)}
+                {makeCard(this.state,6)}
+                {makeCard(this.state,7)}
+                {makeCard(this.state,8)}
+                {makeCard(this.state,9)}
             </div>       
         );
     }
