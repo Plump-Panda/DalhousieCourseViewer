28acd667031599e646f6dfb006cf31b24064a422
Joshua Fung <formygit@gmail.com>
2020-09-09 17:16:56 -0700

    Now shows course description and credit hours

--- src/cardRenderer.js
+++ src/cardRenderer.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import { Card } from 'react-bootstrap';
 
-function getCard(code,name){
+function getCard(code,name,description,creditHours){
     return(
         <Card style={{marginBottom: '2.5%'}}>
             <Card.Header>{code}</Card.Header>
             <Card.Body>
                 <Card.Title>{name}</Card.Title>
+                <Card.Text>{creditHours}</Card.Text>
+                 <Card.Text>{description}</Card.Text>
             </Card.Body>
         </Card>
     );
@@ -14,7 +16,7 @@ function getCard(code,name){
 
 export const makeCard = (json, courseLevel) => {
     return json.course.map((course) =>{
-        const {code,name} = course;
+        const {code,name,description,creditHours} = course;
         let check = code.toString().charAt(5);
 
         //This looks hella dumb, probs a better way to do it
@@ -22,61 +24,61 @@ export const makeCard = (json, courseLevel) => {
             //Will get all the courses
             case 0:
                 return(
-                    getCard(code,name)
+                    getCard(code,name,description,creditHours)
                 )
             case 1:
                 if(check === '1'){
                     return(
-                        getCard(code,name)
+                        getCard(code,name,description,creditHours)
                     )
                  }
                  break;
             case 2:
                 if(check === '2'){
                     return(
-                        getCard(code,name)
+                        getCard(code,name,description,creditHours)
                     )
                  }
                  break;
             case 3:
                 if(check === '3'){
                     return(
-                        getCard(code,name)
+                        getCard(code,name,description,creditHours)
                     )
                  }
                  break;
             case 4:
                 if(check === '4'){
                     return(
-                        getCard(code,name)
+                        getCard(code,name,description,creditHours)
                     )
                  }
                  break;
             case 6:
                 if(check === '6'){
                     return(
-                        getCard(code,name)
+                        getCard(code,name,description,creditHours)
                     )
                  }   
                  break;  
             case 7:
                 if(check === '7'){
                     return(
-                        getCard(code,name)
+                        getCard(code,name,description,creditHours)
                     )
                  }
                  break;
             case 8:
                 if(check === '8'){
                     return(
-                        getCard(code,name)
+                        getCard(code,name,description,creditHours)
                     )
                  }
                  break;
             case 9:
                 if(check === '9'){
                     return(
-                        getCard(code,name)
+                        getCard(code,name,description,creditHours)
                     )
                  }
                  break;
